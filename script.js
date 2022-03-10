a712204c8cdbaf880df0dab47f29e9591d66c5c6
Taylor Noj <taylornoj@gmail.com>
2022-03-10 18:13:24 -0500

    add additional query selectors to countdown and start button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole')
+const countdownBoard = document.querySelector('.countdown');
+const startButton = document.querySelector('.startButton');
+
+
